4470fabda7633d9a8dfe7d96a07664354399960c
Pramit356 <pramitshetty10@gmail.com>
2019-10-02 11:36:51 +0530

    Stopword removal and lemmatization experiment

--- venv/preprocessing.py
+++ venv/preprocessing.py
@@ -1,6 +1,7 @@
 import re
 import pickle
 import enchant
+from nltk import pos_tag, wordnet
 from Dictionary.usedictionary import in_trie, find_correct_words
 
 def is_number(n):
@@ -19,7 +20,7 @@ incorrect = 0
 score = 100
 with open("Dictionary/savedict.txt", "rb") as myFile:
     trie = pickle.load(myFile)
-print(essay)
+
 misswords = []
 incorrect_index = []
 
@@ -31,9 +32,10 @@ for el in essay:
             misswords.append(el.lower())
             incorrect_index.append(essay.index(el))
             incorrect+=1
-print(misswords)
+#print(misswords)
 #Deduct scores here
-
+if(incorrect>4):
+    score-=2*incorrect
 
 #Find correct replacement for words
 #to_use_dict = input("Enter the dictionary (US or UK): ")
@@ -45,10 +47,25 @@ else:
     dictionaryUK = enchant.Dict("en_UK")
     corrected_words = find_correct_words((misswords,dictionaryUK))
 
-print(corrected_words)
+#print(corrected_words)
 for i in range(len(corrected_words)):
     essay[incorrect_index[i]] = corrected_words[i]
-print(essay)
-
-
+#print(essay)
 #Replacement done
+
+#Remove stopwords and stemming
+print(essay)
+useless_tags = ['DT', 'PRP', 'PRP$', 'EX', 'IN', 'CC', 'CD', 'TO', 'WP$', 'WP', 'UH']
+lemma = wordnet.WordNetLemmatizer()
+pos_list = pos_tag(essay)
+#print(pos_list)
+for word in pos_list:
+    if word[1] in useless_tags:
+        essay.remove(word[0])
+        #pos_list.remove(word)       #Refined pos list can be used later in grammar check
+    else:
+        print(essay[essay.index(word[0])])
+        print(lemma.lemmatize(word[0]))
+        essay[essay.index(word[0])] = lemma.lemmatize(word[0])
+
+print(essay)
